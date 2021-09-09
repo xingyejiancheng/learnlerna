7018ffea264f3bc518b17a3f25ab2525b58579de
lixing <lixing3@360.cn>
2021-09-09 13:40:04 +0800

    udpate

--- packages/module-1/lib/module-1.js
+++ packages/module-1/lib/module-1.js
@@ -1,8 +1,8 @@
 'use strict';
-var modules = require('module-2')
+var iconsole = require('module-2')
 module.exports = module1;
 
 function module1() {
     // TODO
-    moodules(2222)
+    iconsole(33333)
 }
