5c15ddd016379f8c5963c15578df15ed9ca7c5cf
lixing <lixing3@360.cn>
2021-09-09 10:20:30 +0800

    first commit

---
+++ packages/module-1/lib/module-1.js
@@ -0,0 +1,8 @@
+'use strict';
+var modules = require('module-2')
+module.exports = module1;
+
+function module1() {
+    // TODO
+    moodules(2222)
+}
---
+++ packages/module-2/lib/module-2.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = module2;
+
+function module2(val) {
+    // TODO
+    console.log(val,11111)
+}
